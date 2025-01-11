19bbac723a8f236cda63195f3135f5613d52ee33
Austin Merrill <austinm9506@gmail.com>
2025-01-11 02:58:08 -0500

    Added Cognito in the auth section of the configuration.

--- BlogIO/utils/amplifyConfiguration.js
+++ BlogIO/utils/amplifyConfiguration.js
@@ -1,9 +1,11 @@
 const amplifyConfiguration = {
     Auth: {
-        region: import.meta.env.VITE_REGION,
-        userPoolId: import.meta.env.VITE_USER_POOL_ID,
-        userPoolWebClientId: import.meta.env.VITE_USER_POOL_CLIENT_ID
+        Cognito: {
+            region: import.meta.env.VITE_REGION,
+            userPoolId: import.meta.env.VITE_USER_POOL_ID,
+            userPoolClientId: import.meta.env.VITE_USER_POOL_CLIENT_ID
+        }
     }
-}
+};
 
 export default amplifyConfiguration;
