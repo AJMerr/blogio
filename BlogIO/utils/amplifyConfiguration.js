adb13fe838159ab4d926c73c075d9bc0b0d296df
Austin Merrill <austinm9506@gmail.com>
2025-01-11 03:14:41 -0500

    Added the AWS Client Secret for Cognito to work properly.

--- BlogIO/utils/amplifyConfiguration.js
+++ BlogIO/utils/amplifyConfiguration.js
@@ -3,7 +3,8 @@ const amplifyConfiguration = {
         Cognito: {
             region: import.meta.env.VITE_REGION,
             userPoolId: import.meta.env.VITE_USER_POOL_ID,
-            userPoolClientId: import.meta.env.VITE_USER_POOL_CLIENT_ID
+            userPoolClientId: import.meta.env.VITE_USER_POOL_CLIENT_ID,
+            userPoolClientSecret: import.meta.env.VITE_USER_POOL_CLIENT_SECRET
         }
     }
 };
