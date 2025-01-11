4278b750233ad1211d87e3f8d3c7afd9d75c2bf0
Austin Merrill <austinm9506@gmail.com>
2025-01-10 19:23:14 -0500

    Moved the background image from the root directory to the src directory. Created a simple login and signup page and added the routes into main.jsx. Also imported PicoCSS in the main.jsx file.

--- BlogIO/src/main.jsx
+++ BlogIO/src/main.jsx
@@ -1,13 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import '@picocss/pico'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Homepage from '../routes/Homepage'
+import LogIn from '../routes/LogIn'
+import SignUp from '../routes/SignUp'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Homepage />,
+  },
+  {
+    path: '/login',
+    element: <LogIn />,
+  },
+  {
+    path: '/signup',
+    element: <SignUp />,
   },
 ])
 
