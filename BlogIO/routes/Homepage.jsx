221e935a4af531013b6848977e5aae0a9a110ce2
Austin Merrill <austinm9506@gmail.com>
2025-01-10 19:20:54 -0500

    Copied styles over from the root directory for BlogIO.

--- BlogIO/routes/Homepage.jsx
+++ BlogIO/routes/Homepage.jsx
@@ -1,5 +1,31 @@
 function Homepage() {
-  return <div>Homepage</div>;
+  return (
+    <main className="container-fluid">
+      <div>
+        <nav className="container">
+          <ul><li>BlogIO!</li></ul>
+          <ul>
+            <li><a href="/login">Log In</a></li>
+            <li><a href="/signup">Register</a></li>
+          </ul>
+        </nav>
+      </div>
+      <section className="hero">
+        <div id="heroContent">
+          <h1 id="heroTitle">BlogIO!</h1>
+          <p id="heroTagline">Your AI tool to create amazing blogposts!</p>
+          <div id="auth">
+            <button id="login">
+              <a href="/login">Login</a>
+            </button>
+            <button>
+              <a href="/signup">Register</a>
+            </button>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default Homepage;
