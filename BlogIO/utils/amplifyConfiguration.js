1cea43cb6ce5115b9af4418d17082ddb276eebfb
Austin Merrill <austinm9506@gmail.com>
2025-01-11 02:39:49 -0500

    Added the VITE_ prefix per VITE security rules.

--- BlogIO/utils/amplifyConfiguration.js
+++ BlogIO/utils/amplifyConfiguration.js
@@ -1,8 +1,8 @@
 const amplifyConfiguration = {
     Auth: {
-        region: import.meta.env.REGION,
-        userPoolId: import.meta.env.USER_POOL_ID,
-        userPoolWebClientId: import.meta.env.USER_POOL_CLIENT_ID
+        region: import.meta.env.VITE_REGION,
+        userPoolId: import.meta.env.VITE_USER_POOL_ID,
+        userPoolWebClientId: import.meta.env.VITE_USER_POOL_CLIENT_ID
     }
 }
 
