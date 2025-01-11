4ef755039a4e2709e7814f915d8d9613302e1c07
Austin Merrill <austinm9506@gmail.com>
2025-01-11 03:28:10 -0500

    Removed Client Secret due to complexity.

--- BlogIO/utils/amplifyConfiguration.js
+++ BlogIO/utils/amplifyConfiguration.js
@@ -3,8 +3,7 @@ const amplifyConfiguration = {
         Cognito: {
             region: import.meta.env.VITE_REGION,
             userPoolId: import.meta.env.VITE_USER_POOL_ID,
-            userPoolClientId: import.meta.env.VITE_USER_POOL_CLIENT_ID,
-            userPoolClientSecret: import.meta.env.VITE_USER_POOL_CLIENT_SECRET
+            userPoolClientId: import.meta.env.VITE_USER_POOL_CLIENT_ID
         }
     }
 };
